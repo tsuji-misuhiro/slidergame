03190042a3a4407e5d30312bf57665b621ed2ca5
tsuji-mitsuhiro <ptmkxg5994@gmail.com>
2024-02-07 22:20:24 +0900

    テストプログラムを動かす

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -8,7 +8,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(num);
     }
 
     // Update is called once per frame
